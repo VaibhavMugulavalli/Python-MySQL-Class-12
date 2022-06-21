f9134479f046cfd3279917bf11aacd60c516fa37
VaibhavMugulavalli <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-06-21 09:17:41 +0000

    Pandas Prac 21/06

---
+++ Padas_I/Q11.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+A1=np.linspace(20,45,2)
+print(A1)
+s=pd.Series(A1,index=['I','II','III','IV','V','VI','VII','VIII','IX','X'])
+print(s)
--- Padas_I/Q8,Q9&Q10.py
+++ Padas_I/Q8,Q9&Q10.py
@@ -1,5 +1,10 @@
+#8
 import pandas as pd
 PriceData=[23000,34500,56000,45000]
 Item=['TV','FRIDGE','OVEN','LAPTOP']
 s=pd.Series(PriceData,index=Item)
 print(s)
+#9
+print(s['FRIDGE'])
+#10
+print(s[2])
