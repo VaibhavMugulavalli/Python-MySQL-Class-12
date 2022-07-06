576f99fd5a743f3e941398da92119ca23c843847
VaibhavMugulavalli <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-07-06 06:24:00 +0000

    Pracs

--- Class 12/Padas_I/DataFrame Programs/Q1.py
+++ Class 12/Padas_I/DataFrame Programs/Q1.py
@@ -15,3 +15,7 @@ print(Result)
 Places=pd.Series(['b','TajMahal','Summer Palce','Qutb Minar','Charminar','Red Fort','Gol Gumbaz'])
 Monuments=pd.DataFrame({'Name':Places,'Year':[1632,1791,1981,1591,1638,1659],'Maker':['Shah Jahan','Tipu Sultan','Qutab-ud-din Aibak','Muḥammad Qulī Quṭb Shah','Shah Jahan','Adil Shahi']},index=[1,2,3,4,5,6])
 print(Monuments)
+#e
+Country={'Country':['India','USA','Russia','China','UK','France','Pakistan','Germany','Italy','Malaysia'],'National Animals':['Bengal Tiger','Bald Eagle','Brown Bear','Giant Panda','Unicorn','Rooster','Markhor','Eagle','Wolf','Malayan Tiger'],'National Bird':['Peakock','Bald Eagle','Double Headed Eagle','Red Crowned Crane','Robin','Rooster','Chukar','Eagle','Sparrow','Hornbill'],'Currency':['Ruppee','Dollar','Rouble','Yuan','Sterling Pound','Euro','Pakistani Ruppee','Euro','Euro','Ringgit']}
+df3=pd.DataFrame(Country)
+print(df3)
