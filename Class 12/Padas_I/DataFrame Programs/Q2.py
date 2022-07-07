9de42dd53f300543fc9db8b4023c260c6523087e
VaibhavMugulavalli <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-07-07 05:25:43 +0000

    Pracs

---
+++ Class 12/Padas_I/DataFrame Programs/Q2.py
@@ -0,0 +1,4 @@
+import pandas as pd
+Grades={'StudentNames':['Sharad','Mansi','Kanika','Ramesh','AnkitA','Pranay'],'UT1':[57,86,92,52,93,98],'HalfYearly':[83,67,78,84,75,79],'UT2':[49,87,45,55,87,88],'Annual':[89,90,66,78,69,96]}
+df1=pd.DataFrame(Grades)
+print(df1)
