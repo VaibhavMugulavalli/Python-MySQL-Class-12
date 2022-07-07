d9c163385d7b48be8673e6a32252a5c5a9d0fd3f
VaibhavMugulavalli <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-07-07 05:41:55 +0000

    Pracs

--- Class 12/Padas_I/DataFrame Programs/Q2.py
+++ Class 12/Padas_I/DataFrame Programs/Q2.py
@@ -1,4 +1,6 @@
 import pandas as pd
 Grades={'StudentNames':['Sharad','Mansi','Kanika','Ramesh','AnkitA','Pranay'],'UT1':[57,86,92,52,93,98],'HalfYearly':[83,67,78,84,75,79],'UT2':[49,87,45,55,87,88],'Annual':[89,90,66,78,69,96]}
-df1=pd.DataFrame(Grades)
+df1=pd.DataFrame(Grades,index=[1,2,3,4,5,6])
+print(df1)
+df1.rename({'Ut1':'Term1','HalfYearly':'Term2','Ut2':'Term3','Annual':'Term4'},axis='column')
 print(df1)
