ce4a5e267bc999dcd60b060ff48ff6e7a44068d4
VaibhavMugulavalli <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-07-12 09:17:50 +0000

    Record

---
+++ Class 12/RecordPrograms/P5.py
@@ -0,0 +1,6 @@
+import pandas as pd
+lst=[1,2,3,4,5,6,7,8,9,10]
+DF1=pd.DataFrame(lst,index=[1,2,3,4,5,6,7,8,9,10])
+print("Sort by:",'\n',"1.Values",'\n',"2.Index")
+print(int(input("Enter option:")))
+if i==1:
