163d3677621c9ccfdc2a212e43de7732bcf8a01d
VaibhavMugulavalli <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-07-12 09:07:47 +0000

    Record

--- Class 12/RecordPrograms/P4.py
+++ Class 12/RecordPrograms/P4.py
@@ -2,4 +2,7 @@ import pandas as pd
 import numpy as np
 dict={'Ram':150,'Rahul':145,'Rohit':122,'Raghav':130,'Ramith':160,'Romith':125,'Rakul':130,'Roman':90,'Raygan':95,'Rakshit':160,'Roshan':85,'Raj':160,'Ruthu':130,'Rancho':160,'Rafthar':100,'Rajath':150,'Rakshita':130,'Romeo':110,'Ramesh':160,'Suresh':160}
 sales=pd.Series(dict)
+sales.sort_values()
+print("Lowest sales:",sales.head)
+print("Higest sales:",sales.tail)
 print(sales)
