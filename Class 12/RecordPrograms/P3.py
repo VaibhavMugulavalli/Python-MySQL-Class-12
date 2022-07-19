6d4b0b14ec2b23a74e41f1043af2a08a2a06c1bc
VaibhavMugulavalli <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-07-19 13:57:03 +0000

    Record

--- Class 12/RecordPrograms/P3.py
+++ Class 12/RecordPrograms/P3.py
@@ -2,12 +2,10 @@ import pandas as pd
 li=[]
 ele=''
 n=int(input("Enter number of areas you want in the series:"))
-
 for i in range(n):
     ele=input("Enter Area:")
     li.append(ele)
-
 s3=pd.Series(li)
 s3.sort_values()
-print("Smallest three areas",s3.tail)
-print("Largest three areas",s3.head)
+print("Smallest areas",s3.tail)
+print("Largest areas",s3.head)
