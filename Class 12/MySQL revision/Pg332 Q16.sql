9e1cf94f974e844f4da6646bacb9691da863300d
MetalHead <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-10-11 06:16:07 +0000

    Revision

---
+++ Class 12/MySQL revision/Pg332 Q16.sql
@@ -0,0 +1,17 @@
+create table student(T_ID int primary key,
+Name varchar(20,
+Age int,
+Department varchar(20),
+Date_of_join date,
+Salary int,
+Gender varchar(8));
+insert into student(T_ID,Name,Age,Department,Date_of_join,Salary,Gender)
+values(1,'Jugal',34,'Computer Sc','2017-01-10',12000,'M'),
+values(2,'Sharmila',31,'History','2008-03-24',20000,'F'),
+values(3,'Sandeep',32,'Mathematics','2016-12-12',30000,'M'),
+values(4,'Sangeeta',35,'History','2015-07-01',40000,'F'),
+values(5,'Rakesh',42,'Mathematics','2007-09-05',25000,'M'),
+values(6,'Shyam',50,'History','2008-06-27',30000,'M'),
+values(7,'ShivOM',44,'Computer Sc','2017-02-25',21000,'M'),
+values(8,'Shalakha',33,'Mathematics','2018-07-31',20000,'F');
+
