dd50641763011a76fe977b4a18ae8246542c4370
MetalHead <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-11-01 13:30:06 +0000

    Project

--- Class 12 Project/scrapper.py
+++ Class 12 Project/scrapper.py
@@ -1,5 +1,40 @@
 import pandas as pd #importing pandas library
-url='https://en.wikipedia.org/wiki/Formula_One' #Assigning url
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import re
+
+
+def __initTableTitles__(content):
+    # Finds all containers having tag = table
+    tables=content.findAll('table')
+    captionList={}
+    count=1
+    
+    for table in tables:
+        # Querying through each table to find all <caption></caption> tags which is conventionally used to assign table title
+        captions = list(table.findAll('caption'))
+        # Handler for captions returning None/empty list
+        if len(captions):
+            caption = captions[0]
+            # Removes the "\n" at the end of the caption string
+            stripNewLine = str(caption.getText())[:-1]
+        else:
+            stripNewLine = " "
+        # removes all refs like [8][9] by splitting it and then taking the first one
+        captionRefSplit = stripNewLine.split("[")
+        cleanCaption = captionRefSplit[0]
+        # adds to caption dictionary
+        captionList[count] = cleanCaption
+        count +=1
+    # assgins tableTitles attribute to object
+
+    return captionList
+
+
+url=input("Enter valid url:") #Assigning url
+
+
 df=pd.read_html(url) #Reading html format of url
 n=len(df) #Finding number of tables in url
 print("Number of tables in webpage:",n)
