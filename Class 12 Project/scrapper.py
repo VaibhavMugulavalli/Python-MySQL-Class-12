487f0d8a3f1d5c0a54eb3a2bd4855775a52a588f
MetalHead <85826457+VaibhavMugulavalli@users.noreply.github.com>
2022-12-07 11:15:11 +0530

    danke himan

--- Class 12 Project/scrapper.py
+++ Class 12 Project/scrapper.py
@@ -1,39 +1,31 @@
-import pandas as pd #importing pandas library
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+import pandas as pd #importing pandas
+import urllib.request #importing urllib
+from bs4 import BeautifulSoup #importing bs4
 
-def initTableTitles(content):
-    # Finds all containers having tag = table
+def initTableTitles(content): # Finds all containers having tag=table
     tables=content.findAll('table')
     captionList={}
     count=1
     
-    for table in tables:
-        # Querying through each table to find all <caption></caption> tags which is conventionally used to assign table title
-        if(not len(table.find('thead'))):
+    for table in tables:# Querying through each table to find all <caption></caption> tags which is commonly used to assign table title
+        if(not len(table.find('thead'))): #finding <thead></thead> tags(used to group header content under a table)
             break
         captions = list(table.findAll('caption'))
-        print(captions)
-        # Handler for captions returning None/empty list
+        print(captions)# Handler for captions returning None/empty list
         if len(captions):
-            caption = captions[0]
-            # Removes the "\n" at the end of the caption string
+            caption = captions[0]# Removes the "\n" at the end of the caption string
             stripNewLine = str(caption.getText())[:-1]
         else:
-            stripNewLine = " "
-        # removes all refs like [8][9] by splitting it and then taking the first one
+            stripNewLine = " " # removes all refrencess like [1],[2] by splitting it and then taking the first one
         captionRefSplit = stripNewLine.split("[")
-        cleanCaption = captionRefSplit[0]
-        # adds to caption dictionary
+        cleanCaption = captionRefSplit[0] # adds to caption dictionary
         captionList[count] = cleanCaption
-        count +=1
-    # assgins tableTitles attribute to object
+        count +=1# assgins tableTitles attribute to object
 
     return captionList
 
-def cleanDFs(url,content):
-    captionlist=initTableTitles(content)
-    # Gets all tables in webpage
+def cleanDFs(url,content): #Used to clean data from dirty DF's
+    captionlist=initTableTitles(content)# Gets all tables in webpage
     dirtyDF = pd.read_html(url)
     dfDict = {}
     cleanedTableTitles = []
@@ -47,24 +39,23 @@ def cleanDFs(url,content):
     
     return [CleanTable,cleanedTableTitles]
 
-def getTable(url,content):
+def getTable(url,content): #Used to retrieve cleandata(filtered out from Dirty Df's)
     cleaneddata=cleanDFs(url,content)
     print("-------TABLES--------")
     tables=cleaneddata[0]
     captions = cleaneddata[1]
     print(captions)
-    count = 1
-    # Prints the options to use for each table
-    for title in captions:
+    count = 1 
+    for title in captions: #Prints the options to use for each table
         print(f"{count} for {title}")
         count +=1
     num=int(input("Enter table number:"))
     return tables[captions[num-1]]
 
 
-url=input("Enter valid url:") #Assigning url
+url=input("Enter valid url:") #User input for URL
 response = urlopen(url)
-content = BeautifulSoup(response.read(), 'html.parser')
+content = BeautifulSoup(response.read(), 'html.parser') #parsing URL 
 
 requireddf=getTable(url,content)
 print(".CSV file:",requireddf.to_csv('Scrapped Table CSV')) #Exporting data to CSV file
